b35b413b0777142c19d57fe61ab148d56b1bfc86
jonathan-innis <jonathan.innis.ji@gmail.com>
2018-05-28 21:58:52 -0700

    Completely changed the design

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-$(function(){                  
-    $(window).scroll(function(){                          
-        if ($(this).scrollTop() > 200) {
-            $('#menu').fadeIn(500);
-        } else {
-            $('#menu').fadeOut(500);
-        }
-    });
-})
