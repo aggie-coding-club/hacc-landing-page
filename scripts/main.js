66dddf101a240656b5ffda44b179df47b255516b
jonathan-innis <jonathan.innis.ji@gmail.com>
2018-09-05 15:01:34 -0500

    Removed the up movement with animation

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,8 @@ $(function(){
     const sr = ScrollReveal();
 
     //ScrollReveal Parts
-    sr.reveal('.sponsor-logo', { duration: 1500, distance: '10px'}, 50);
-    sr.reveal('.contact-item', { duration: 1500, distance: '10px'}, 50);
+    sr.reveal('.sponsor-logo', { duration: 1500}, 50);
+    sr.reveal('.contact-item', { duration: 1500}, 50);
 
     //Provides the functionality for the expansions of the boxes in the FAQ section
     var acc = document.getElementsByClassName("accordion");
